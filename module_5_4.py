f45096aeb4b5183f4dc8d27ff3d221cb47907005
TereshinaEVa <tereshina.eva@gmail.com>
2024-06-24 22:53:26 +0300

    Различие атрибутов класса и экземпляра-3вариант

--- module_5_4.py
+++ module_5_4.py
@@ -1,13 +1,11 @@
 class Building:
     total = 0
-    def __init__(self, i):
-        self.number = Building.total + i
+    def __init__(self):
+        Building.total += 1
 
 obj = {}
 for i in range(1,41):
    num = 'obj{}'.format(i)
-   obj[num] = Building(i)
-   Building.total = obj[num].number
-   obj[num].total = obj[num].number
+   obj[num] = Building()
    print(f'измененный атрибут класса {Building.total}, ', f'атрибут экземпляра {obj[num].total}.')
 
