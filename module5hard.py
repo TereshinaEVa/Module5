87da3664a8e9d6e9e82d4b5fa76bfc1833cf77d7
TereshinaEVa <tereshina.eva@gmail.com>
2024-11-24 11:04:49 +0300

    Свой YouTube - доделала

--- module5hard.py
+++ module5hard.py
@@ -1,17 +1,14 @@
-import hashlib
 from time import sleep
-
-
-def hash_password(password):
-    hashed = hashlib.sha256(password.encode()).hexdigest()
-    return int(hashed, 16)
 
 
 class User:
     def __init__(self, nickname: str, password: int, age: int):
         self.nickname = nickname
-        self.password = hash_password(password)
+        self.password = password
         self.age = age
+
+    def __str__(self):
+        return self.nickname
 
 
 class Video:
@@ -21,22 +18,76 @@ class Video:
         self.time_now = 0
         self.adult_mode = adult_mode
 
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __contains__(self, item):
+        return item in self.title
+
 
 class UrTube:
-    pass
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def register(self, nickname, password, age):
+        password = hash(password)
+        for user in self.users:
+            if user.nickname == nickname:
+                print(f"Пользователь {nickname} уже существует")
+                return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+
+    def log_out(self):
+        self.current_user = None
+
+    def log_in(self, nickname, password):
+        for user in self.users:
+            if nickname == user.nickname and password == user.password:
+                self.current_user = user
+        print('Неправильный логин или пароль')
+
+    def add(self, *videos):
+        for video in videos:
+            if video not in self.videos:
+                self.videos.append(video)
+
+    def get_videos(self, search_word):
+        search_word = search_word.lower()
+        matching_videos = []
+        for video in self.videos:
+            if search_word in video.title.lower():
+                matching_videos.append(video.title)
+        return matching_videos
+
+    def watch_video(self, movie):
+        if not self.current_user:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+
+        for video in self.videos:
+            if video.title == movie:
+                if video.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+
+                for i in range(video.duration):
+                    print(i+1, end=' ')
+                    video.time_now += 1
+                    #sleep(1)
+                video.time_now = 0
+                print("Конец видео")
 
 
 if __name__ == '__main__':
-    us_data = Database()
-    user = User('Bill', 123456, 20)
-    us_data.add_user(user.nickname, user.password)
-    print(us_data.data)
 
+    ur = UrTube()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-
-ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
