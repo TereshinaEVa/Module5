db173c59d9162fa8964084d38568c642449643bc
TereshinaEVa <tereshina.eva@gmail.com>
2024-11-24 10:12:00 +0300

    Свой YouTube

---
+++ module5hard.py
@@ -0,0 +1,60 @@
+import hashlib
+from time import sleep
+
+
+def hash_password(password):
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    return int(hashed, 16)
+
+
+class User:
+    def __init__(self, nickname: str, password: int, age: int):
+        self.nickname = nickname
+        self.password = hash_password(password)
+        self.age = age
+
+
+class Video:
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+
+class UrTube:
+    pass
+
+
+if __name__ == '__main__':
+    us_data = Database()
+    user = User('Bill', 123456, 20)
+    us_data.add_user(user.nickname, user.password)
+    print(us_data.data)
+
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
